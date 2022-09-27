da9f821dab52b7f708a23b80bd8107e845da7050
Navvya21 <75784090+Navvya21@users.noreply.github.com>
2022-09-27 14:33:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,12 +8,29 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(600,300);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() { 
+	console.log('Model Loaded!'); 
 }
 
 function draw() {
 	game()
 }
 
+function gotPoses(results) {
+	if(results.length > 0){
+		console.log(results);
+	noseX = results[0].pose.nose.x;
+	noseY = results[0].pose.nose.y;
+	}
+  }
 
 
 
